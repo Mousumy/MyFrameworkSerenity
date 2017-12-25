96ed6973941c6bfeadacee3423175daf3124d2d4
Saikat Barua <Saikat Barua>
2017-12-24 20:37:38 -0500

    all facebook scenario added

--- src/test/resources/features/consult_dictionary/ProfilePage.feature
+++ src/test/resources/features/consult_dictionary/ProfilePage.feature
@@ -10,3 +10,38 @@ Background:
 @regression
 Scenario: Search for Friends
   	When User will search for "Sunil C Patel"
+  	
+@regression
+Scenario: Timeline functionality verification
+	When Navigate to "Timeline"	
+	And Verify header "Life Event" is present
+	Then Verify "Work & Education" funtionality should work
+	@regression       
+Scenario: profilepage header verification
+	Then Verify this header "Photos" is present
+	
+@regression
+Scenario: More functionality verification
+	And Navigate to "more"
+	Then verify more functionality is working
+
+@regression
+Scenario: Friends functionality verification
+	And Navigate to "Friends"
+	And User will search for "Sunil C Patel"
+	
+@quick
+Scenario: EditPost functionality verification
+	When User will randomly select post
+	Then "Hide From TimeLine" functionality should work
+	And "ChangeDate" functionality should work
+	And "Hide from timeline" functionality should work	
+
+@regression
+Scenario:	MakePost functionality verification
+		When User will make new post
+		Then It wil be visible in timeline
+@quick		
+Scenario: About Functionality verification
+	When Navigate to "About"
+	Then About tab has this feature option "Work and Education"
--- target/cucumber/report.js
+++ target/cucumber/report.js
@@ -229,4 +229,397 @@ formatter.match({
 formatter.result({
   "status": "passed"
 });
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "User can log in to facebook using userid as \"asmenterprise.llc@gmail.com\" and password as \"ctg310306\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.user_can_log_in_to_facebook_using_userid_as_and_password_as(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User profile has been loaded successfully",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.user_profile_has_been_loaded_successfully()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Timeline functionality verification",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@regression"
+    }
+  ]
+});
+formatter.step({
+  "name": "Navigate to \"Timeline\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.navigate_to(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Verify header \"Life Event\" is present",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.verify_header_is_present(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Verify \"Work \u0026 Education\" funtionality should work",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.verify_funtionality_should_work(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "User can log in to facebook using userid as \"asmenterprise.llc@gmail.com\" and password as \"ctg310306\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.user_can_log_in_to_facebook_using_userid_as_and_password_as(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User profile has been loaded successfully",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.user_profile_has_been_loaded_successfully()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "profilepage header verification",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@regression"
+    }
+  ]
+});
+formatter.step({
+  "name": "Verify this header \"Photos\" is present",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.verify_this_header_is_present(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "User can log in to facebook using userid as \"asmenterprise.llc@gmail.com\" and password as \"ctg310306\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.user_can_log_in_to_facebook_using_userid_as_and_password_as(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User profile has been loaded successfully",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.user_profile_has_been_loaded_successfully()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "More functionality verification",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@regression"
+    }
+  ]
+});
+formatter.step({
+  "name": "Navigate to \"more\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.navigate_to(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "verify more functionality is working",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.verify_more_functionality_is_working()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "User can log in to facebook using userid as \"asmenterprise.llc@gmail.com\" and password as \"ctg310306\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.user_can_log_in_to_facebook_using_userid_as_and_password_as(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User profile has been loaded successfully",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.user_profile_has_been_loaded_successfully()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Friends functionality verification",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@regression"
+    }
+  ]
+});
+formatter.step({
+  "name": "Navigate to \"Friends\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.navigate_to(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User will search for \"Sunil C Patel\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.user_will_search_for(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "User can log in to facebook using userid as \"asmenterprise.llc@gmail.com\" and password as \"ctg310306\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.user_can_log_in_to_facebook_using_userid_as_and_password_as(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User profile has been loaded successfully",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.user_profile_has_been_loaded_successfully()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "EditPost functionality verification",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@quick"
+    }
+  ]
+});
+formatter.step({
+  "name": "User will randomly select post",
+  "keyword": "When "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "\"Hide From TimeLine\" functionality should work",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "\"ChangeDate\" functionality should work",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "\"Hide from timeline\" functionality should work",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "User can log in to facebook using userid as \"asmenterprise.llc@gmail.com\" and password as \"ctg310306\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.user_can_log_in_to_facebook_using_userid_as_and_password_as(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User profile has been loaded successfully",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.user_profile_has_been_loaded_successfully()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "MakePost functionality verification",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@regression"
+    }
+  ]
+});
+formatter.step({
+  "name": "User will make new post",
+  "keyword": "When "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "It wil be visible in timeline",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "User can log in to facebook using userid as \"asmenterprise.llc@gmail.com\" and password as \"ctg310306\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.user_can_log_in_to_facebook_using_userid_as_and_password_as(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User profile has been loaded successfully",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.user_profile_has_been_loaded_successfully()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "About Functionality verification",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@quick"
+    }
+  ]
+});
+formatter.step({
+  "name": "Navigate to \"About\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.navigate_to(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "About tab has this feature option \"Work and Education\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ProfilePageStepDef.about_tab_has_this_feature_option(String)"
+});
+formatter.result({
+  "status": "passed"
+});
 });
